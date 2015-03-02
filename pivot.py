517ab9c17adb029f7a2a8be6bf1b2f54d2abc8b5
t0mst0ne <iamtomstone@gmail.com>
2015-03-02 11:25:36 +0800

    new file:   C2_by_day.csv 	new file:   C2_sum.csv 	modified:   pivot.py

--- pivot.py
+++ pivot.py
@@ -3,10 +3,20 @@ import sys
 import numpy as np
 
 df = pd.io.parsers.read_csv(sys.stdin)
-print df.head(5)
 df.columns = ["ID","YM","Date","C1", "C2"]
-df2 = pd.pivot_table(df,index=["Date"],columns=['ID'],aggfunc=[np.sum],values=['C1'],margins=False,fill_value=0) 
-df2 = df2['sum']['C1']
-df2.to_csv('latest_by_day.csv')
+df1 = pd.pivot_table(df,index=["Date"],columns=['ID'],aggfunc=[np.sum],values=['C1'],margins=False,fill_value=0) 
+df1 = df1['sum']['C1']
+df1.to_csv('latest_by_day.csv')
+df1_cum = df1.cumsum()
+df1_cum.to_csv('latest_sum.csv')
+
+print 'C1 done'
+
+# Check C2
+df2 = pd.pivot_table(df,index=["Date"],columns=['ID'],aggfunc=[np.sum],values=['C2'],margins=False,fill_value=0) 
+df2 = df2['sum']['C2']
+df2.to_csv('C2_by_day.csv')
 df2_cum = df2.cumsum()
-df2_cum.to_csv('latest_sum.csv')
+df2_cum.to_csv('C2_sum.csv')
+
+print 'C2 done'
