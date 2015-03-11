fd77391ea5b30e59bee69f08b990ba33bfad53d5
t0mst0ne <iamtomstone@gmail.com>
2015-03-11 21:11:53 +0800

    add re.IGNORECASE 	modified:   order.item.stat.py

--- order.item.stat.py
+++ order.item.stat.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import matplotlib
 import numpy as np
+import re
 
 # matplotlib SETTING
 matplotlib.rcParams["font.family"] = "SimHei"
@@ -10,17 +11,11 @@ matplotlib.use('Agg')
 
 xl = pd.read_excel("20150101-20150308 list.xls")
 
-Order = ['Port-|port-','Foley|foley|foly|Forley','enema','PP care','cystofix','Bladder','lumbar puncture','ICP',
-        'pump','ON IV|On IV|on IV|ON iv','IABP','pulse ox','pacemaker|Pacemaker','monitor',
-        'Tracheo','steam|Stream','CPT','suct|Suct','On Endo|on Endo|on endo| On endo|On ETT|on ETT','On O2',
-        'Restra','ON NIPPV|On NIPPV','IPPB','high flow','CO2','ambu bagging|Ambu bagging','apnea',
-        'Skin test|PCT test|PPD test','chest tube','pig tail',
-        'on pig|On pig','EFM','NST','TPN','heat lamp|Heat lamp','CPM','ROM',
-        'Phototh','ncubator','(\d)U|(\d)u','Double','CVP','A-line','sit bath|Sit bath','packing','shav|Shav','NG|N\/G|(\s+)OG(\s+)']
+Order = ['Port-','Foley|foly|Forley','enema','PP care','cystofix','Bladder','lumbar puncture','ICP', 'pump','ON IV','IABP','pulse ox','Pacemaker','monitor', 'Tracheo','Stream','CPT','Suct','On Endo| On endo|On ETT','On O2', 'Restra','ON NIPPV','IPPB','high flow','CO2','Ambu bagging','apnea', 'Skin test|PCT test|PPD test','chest tube','pig tail', 'On pig','EFM','NST','TPN','heat lamp','CPM','ROM', 'Phototh','ncubator','(\d)U|(\d)u','Double','CVP','A-line','Sit bath','packing','Shav','NG|N\/G|(\s+)OG(\s+)']
 keys = [u'新增人員']
 
 for txt in Order:
-    selection = xl[ xl[u'醫囑'].str.contains(txt).fillna(False) ]
+    selection = xl[ xl[u'醫囑'].str.contains(txt,re.IGNORECASE).fillna(False) ]
     pt = pd.pivot_table(selection, values=u'筆數', index=[u'年月'],columns=[u'新增人員'], aggfunc=np.sum,fill_value=0)
     pt.plot(pt.index,title=txt,legend=False,kind='bar')
     matplotlib.pyplot.savefig('img/2015.Jan-Mar_by_month_' + str(txt) + '.png')
